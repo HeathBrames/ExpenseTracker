3216b5b8d7891d5350a4e63980cbdd97bb247640
Heath Brames <55902552+HeathBrames@users.noreply.github.com>
2020-07-14 22:23:14 -0400

    Initial File upload

---
+++ AppReducer.js
@@ -0,0 +1,18 @@
+//reducer specifies applications state changes based on actions to context
+//a way to change your state then send it down through your application
+
+export default (state, action) => {
+    switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return{
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return{
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+        default: return state;
+    }
+}
